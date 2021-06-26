0ab8e9454f026a04f196a0bc2cdfc14541637682
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-26 15:39:50 -0500

    se eliminan documentos

--- main.js
+++
@@ -1,7 +0,0 @@
-const altura = document.body.scrollHeight - window.innerHeight;
-const fondo = document.getElementById ('fondo');
-
-window.onscroll = () => {
-    const anchofondo = (window.pageYOffset / altura) * 900;
-    fondo.style.width = anchofondo + '%';
-} 
